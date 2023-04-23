d8ffab79e615741a2d1c6578ebc5b3f6b678c011
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2023-04-23 22:47:39 +0000

    added a responsive layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
       title: 'Instagram Clone',
       debugShowCheckedModeBanner: false,
       home: Scaffold(
