e8899d426ee70dc43d5ab4af86ac6598216aa0af
Jonathan Wilchield Arthur(10945544) <100198275+jocypher@users.noreply.github.com>
2023-04-24 00:11:27 +0000

    files added

--- lib/responsive/webScreenLayout.dart
+++ lib/responsive/webScreenLayout.dart
@@ -5,6 +5,8 @@ class WebScreenLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: Center(child: Text("This is a Web Screen"),),
+    );
   }
 }
